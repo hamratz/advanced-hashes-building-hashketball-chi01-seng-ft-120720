0b7766f32fef95f824d436663a9d2c7cf8f0d4e5
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 16:17:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,7 +36,7 @@ def game_hash
               rebounds: 19,
               assists: 10,
               steals: 3,
-              blocks: 8,
+              blocks: 1,
               slam_dunks: 15
               },
               {
