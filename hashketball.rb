4ca5457244326b12f20b1d4e7beca8b41d463b0d
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 16:07:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,22 +1,23 @@
 # Write your code here!
-game_hash{
+game_hash
+    {
 
-  home: {
-    team_name: "Brookly Nets"
-    colors: ["Black", "White"]
-    players: [ 
-      {
-      player_name:"Alan Anderson"
-      number: 0,
-      shoe: 16,
-      points: 22,
-      rebounds: 12,
-      assists: 12,
-      steals: 3,
-      blocks: 1,
-      slam_dunks: 1
-      },
-      {
+       home: {
+          team_name: "Brookly Nets"
+          colors: ["Black", "White"]
+          players: [ 
+              {
+              player_name:"Alan Anderson"
+              number: 0,
+              shoe: 16,
+              points: 22,
+              rebounds: 12,
+              assists: 12,
+              steals: 3,
+              blocks: 1,
+              slam_dunks: 1
+              },
+              {
       player_name:"Reggie Evans"
       number: 30,
       shoe: 14,
