78df4bf749767da8914f9691ce56ccf3462835c6
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 15:44:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,29 @@
 # Write your code here!
+game_hash
+
+:home
+  :team_name
+  :colors 
+  :players 
+    :player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks
+:away
+  :team_name
+  :colors 
+  :players 
+    :player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks
